04cb2ddb0ea1709671208422354d7d5540a993fb
MuxaTim <muxatim@gmail.com>
2019-02-03 15:21:24 +0700

    Выполнил ДЗ №3:
    Написать программу обмена значениями двух целочисленных переменных:
    a. с использованием третьей переменной;
    b. *без использования третьей переменной.

--- Lesson01/Lesson01/Source.c
+++ Lesson01/Lesson01/Source.c
@@ -10,18 +10,18 @@ int main(int argc, const char * argv[])
 {
 	printf("Student: Timchenko Mikhail\nAlgorithms and data structures. Basic course.\nHomework #1\n\n");
 
+	//Задача №1 - калькулятор индекса массы тела
 	printf("01. BMI Calculator.\n\n");
-	double weight;
-	double height;
-	double bmi;
+	float weight, height, bmi;
 	printf("Input your weight:");
-	scanf("%lf", &weight);
+	scanf("%f", &weight);
 	printf("Input your height:");
-	scanf("%lf", &height);
+	scanf("%f", &height);
 
 	bmi = BMICalculator(weight, height);
-	printf("Yor BMI is: %.2lf", bmi);
+	printf("Yor BMI is: %.2f", bmi);
 
+	//Задача №2 - найти максимум из 4 чисел
 	printf("\n\n02. Maximum.\n\n");
 	double a, max;
 	int counter = 0;
@@ -38,6 +38,32 @@ int main(int argc, const char * argv[])
 		counter++;
 	}
 	printf("Maximum: %lf", max);
+
+	//Задача №3 - обмен значениями
+	printf("\n\n03. Replacing values.\n\n");
+	int b1, b2, c;
+	printf("%s", "Enter b1 (integer):");
+	scanf("%d", &b1);
+
+	printf("%s", "Enter b2 (integer):");
+	scanf("%d", &b2);
+
+	c = b1;
+	b1 = b2;
+	b2 = c;
+
+	printf("Replacing values using the third variable:\n");
+	printf("b1 = %i\n", b1);
+	printf("b2 = %i\n", b2);
+
+	b1 = b1 + b2;
+	b2 = b1 - b2;
+	b1 = b1 - b2;
+
+	printf("Replacing values without using the third variable:\n");
+	printf("b1 = %i\n", b1);
+	printf("b2 = %i\n", b2);
+
 
 	getch();
 	return 0;
